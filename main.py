f625a9d3126bbeac5f190f0a16ddacbcd3de8ab4
mohamadrasool0102-cell <mohamad.rasool.01.02@gmail.com>
2026-07-30 10:17:41 +0330

    Create main.py

---
+++ main.py
@@ -0,0 +1,18 @@
+from telegram import Update
+from telegram.ext import Application, CommandHandler, MessageHandler, ContextTypes, filters
+import os
+
+TOKEN = os.getenv("BOT_TOKEN")
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("سلام! من NovaSmartBot هستم 🤖")
+
+async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text(update.message.text)
+
+app = Application.builder().token(TOKEN).build()
+app.add_handler(CommandHandler("start", start))
+app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
+
+print("Bot is running...")
+app.run_polling()
